1aeb2b1f36e45e58d126bb2f04036164fa56914f
Oscar Xiao <oscarxiao115@gmail.com>
2020-02-27 18:24:25 -0800

    Add p1 folder and examples in practice folder

---
+++ practice/script.js
@@ -0,0 +1,22 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    item: [{ quantity: null, name: null }],
+    itemNum: null,
+    itemName: null
+
+    // item: [{ quantity: null, name: null }],
+    // numEach: null,
+  }
+  // computed: {
+  //   total() {
+  //     let totalNum = this.itemLists.length;
+  //     return totalNum;
+  //   }
+  // method:{
+  //   addItem(){
+  //     this.item.push({quantity: this.numEach})
+  //     this.item.push({name: this.itemName})
+
+  //   }
+});
