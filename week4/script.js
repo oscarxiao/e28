1d65d7cf2aa65fbc6f84b37b03fdfafb48edbe2e
Oscar Xiao <oscarxiao115@gmail.com>
2020-03-04 12:12:55 -0800

    Added week4 folder with index.html tryingto solve shopping list

---
+++ week4/script.js
@@ -0,0 +1,21 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    itemAmount: null,
+    itemName: null,
+    theItem: {},
+    items: []
+  },
+  methods: {
+    combineAmountAndName: function() {
+      let itemWillBeInItems = {};
+      itemWillBeInItems.amount = this.itemAmount;
+      itemWillBeInItems.name = this.itemName;
+      this.items.push(itemWillBeInItems);
+      //return this.theItem;
+    }
+    // submitItem: function() {
+    //   this.items.push(this.theItem);
+    //   return this.items;
+  }
+});
