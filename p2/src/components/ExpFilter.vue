82cf94a77ba480b06d91a5fac634e78d9e3ede89
Oscar Xiao <oscarxiao115@gmail.com>
2020-04-26 20:37:02 -0700

    Added data chart in ExpFilter.vue with hard-coded data

--- p2/src/components/ExpFilter.vue
+++ p2/src/components/ExpFilter.vue
@@ -4,17 +4,25 @@
     <ul>
       <li v-for="exp in FilterdExperiments" :key="exp.id">{{ exp.name }}</li>
     </ul>
+    <!-- Chart insert -->
+    <LineChart :chart-data="dataCollection"></LineChart>
   </div>
 </template>
 
 <script>
 import { exps } from "./../experiments.js";
+import LineChart from "./../DataChart.js";
 export default {
   name: "",
   props: ["description"],
+  components: {
+    LineChart,
+  },
   data: function() {
     return {
-      exps: exps
+      exps: exps,
+      dataCollection: null,
+      loaded: false,
     };
   },
   computed: {
@@ -23,10 +31,30 @@ export default {
         return exp.description.includes(this);
       }
       return this.exps.filter(isMatch, this.description);
-    }
-  }
+    },
+  },
+  mounted: function() {
+    this.fillData();
+  },
+  methods: {
+    fillData: function() {
+      this.dataCollection = {
+        labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        datasets: [
+          {
+            label: "cell count #",
+            backgroundColor: "rgba(255,0, 0)",
+            borderColor: "lightblue",
+            pointBackgroundColor: "blue",
+            borderWidth: 1,
+            pointBorderColor: "blue",
+            data: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+          },
+        ],
+      };
+    },
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
