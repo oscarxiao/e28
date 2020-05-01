ca3121752be00b367b0d1a54eb0153d48d9c54ae
Oscar Xiao <oscarxiao115@gmail.com>
2020-05-01 15:26:44 -0700

    delected comment in ShowExp.vue

--- p2/src/components/ShowExp.vue
+++ p2/src/components/ShowExp.vue
@@ -42,17 +42,6 @@
         </div>
       </div>
     </div>
-
-    <!-- <div>{{ exp.name }}</div>
-    <p>{{ exp.description }}</p>
-    <div>{{ exp.pepA }}</div>
-    <div>{{ exp.pepB }}</div>
-    <div>{{ exp.pepC }}</div>
-    <div>{{ exp.pepD }}</div>
-    <div>{{ exp.pepZero1 }}</div>
-    <div>{{ exp.pepZero2 }}</div>
-    <div>{{ exp.pop1ug }}</div>
-    <div>{{ exp.pairSeq }}</div>-->
   </div>
 </template>
 
