b9074167d0ef00cfeeceec7a33a56abe08eca7fa
Oscar Xiao <oscarxiao115@gmail.com>
2020-04-30 17:15:09 -0700

    Fixed the all experiment issues and ready for production website

--- p2/src/components/ShowExps.vue
+++ p2/src/components/ShowExps.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="experiments">
     <h2>All Experiments</h2>
-    <button @click="getAllExperiments">Get All Experiments</button>
+    <button @click="getAllExps">Get All Experiments</button>
     <show-exp v-for="exp in exps" :key="exp.id" :exp="exp"></show-exp>
   </div>
 </template>
@@ -16,26 +16,26 @@ export default {
   },
   data: function() {
     return {
-      exps: null,
+      exps: [],
     };
   },
   mounted: function() {
-    console.log("Mounted working!");
-    this.getAllExps;
-    this.getAllExperiments;
+    // console.log("Mounted working!");
+    // this.getAllExps();
+    // this.getAllExperiments();
   },
   methods: {
     getAllExps: function() {
       console.log("getAllExps function working");
-      app.api.all("allExperiments");
-      // .then((response) => {
-      //   this.exp = response;
-      //   console.log(response);
-      // });
+      app.api.all("allExperiments").then((response) => {
+        //   this.exp = response;
+        // console.log(Object.values(response));
+        this.exps = Object.values(response);
+      });
     },
-    getAllExperiments: function() {
-      console.log("getAllExperiments function working");
-    },
+    // getAllExperiments: function() {
+    //   console.log("getAllExperiments function working");
+    // },
   },
 };
 </script>
